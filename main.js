621b867ef7b6d9773508c89e1289befc606d522e
Rafael Dalepiane <rafadalepi@gmail.com>
2022-11-04 16:05:13 -0300

    Submit Button changes with right text typed

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 var myInput = document.getElementById("password");
-let screenWidth = screen.width / 2;
 let submitButton = document.getElementById('submitButton');
 
-myInput.onmousemove = function(){
-    var caracters = /[a-z]/g /* && /[A-Z]/g && /[0-9]/g && /\W|_/g */;
+myInput.onkeypress = function(){
+	var characters = /[a-z]/g /* && /[A-Z]/g && /[0-9]/g && /\W|_/g */;
     
-    if (myInput.value.match(caracters) /* && myInput.value.length >= 8  */){
-        myInput.style.backgroundColor = "#8be78b"
+    if(myInput.value.match(characters) && myInput.value.length >= 8){
+        
+		myInput.style.backgroundColor = "#8be78b"
 		submitButton.style.left = 50 + '%';
-		console.log("a")
-    }else{
-        myInput.style.backgroundColor = "#f1a9a9"
-		
-		const onMouseMove = (e) =>{
-			let left = e.offsetX;
-			if( left > screenWidth ){
-				submitButton.style.left = e.pageX - 150 + 'px';
-			}else{
-				submitButton.style.left = e.pageX + 150 + 'px';
-			}
-		}
-		document.addEventListener('mousemove', onMouseMove);
-		console.log("b")
-    }
+
+	}else{
+		myInput.style.backgroundColor = "#f1a9a9"
+		func();
+	}
+}
+
+var func = () => {
+	submitButton.onmouseover = evento;
+}
+
+var iterator = 0;
+
+var evento = (mouseEvent) => {
+	
+	iterator++;
+	iterator = iterator % 2;
+	
+	if(iterator === 0 ) {
+		submitButton.style.left = mouseEvent.pageX - 150 + 'px';
+	}
+	if(iterator === 1){
+		submitButton.style.left = mouseEvent.pageX + 150 + 'px';
+	}
 }
