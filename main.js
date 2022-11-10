c7d306070f45d9c6a7e850a0933cf319adc9410e
Rafael Dalepiane <rafadalepi@gmail.com>
2022-11-10 10:28:06 -0300

    Working project - visual bugs

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
 var myInput = document.getElementById("password");
 let submitButton = document.getElementById('submitButton');
+let iterator = 0;
 
-myInput.onkeypress = function(){
-	var characters = /[a-z]/g /* && /[A-Z]/g && /[0-9]/g && /\W|_/g */;
-    
-    if(myInput.value.match(characters) && myInput.value.length >= 8){
-        
+
+myInput.onkeydown = function(){
+	let characters = /[a-z]/g /* && /[A-Z]/g && /[0-9]/g && /\W|_/g */;
+	
+	if(myInput.value.match(characters) && myInput.value.length >= 8){
 		myInput.style.backgroundColor = "#8be78b"
-		submitButton.style.left = 50 + '%';
-
+		submitButton.onmouseenter = () => {
+			submitButton.style.left;
+		}
 	}else{
 		myInput.style.backgroundColor = "#f1a9a9"
-		func();
-	}
-}
-
-var func = () => {
-	submitButton.onmouseover = evento;
-}
-
-var iterator = 0;
-
-var evento = (mouseEvent) => {
-	
-	iterator++;
-	iterator = iterator % 2;
-	
-	if(iterator === 0 ) {
-		submitButton.style.left = mouseEvent.pageX - 150 + 'px';
-	}
-	if(iterator === 1){
-		submitButton.style.left = mouseEvent.pageX + 150 + 'px';
+		console.log("teste");
+		submitButton.onmouseenter = (mouseEnter) =>{
+			iterator++;
+			iterator = iterator % 2;
+			if(iterator === 0 ) {
+				submitButton.style.left = mouseEnter.pageX - 150 + 'px';
+			}else if(iterator === 1){
+				submitButton.style.left = mouseEnter.pageX + 150 + 'px';
+			}
+		}
 	}
 }
